bfd72dc654897f811fdd6f7a0a3ec4ee2ce8a694
aronfinkelstein <finkelstein.aron@gmail.com>
2025-02-24 09:15:48 +0000

    adding notebooks, removing clutter

--- src/vehicle_models/energy_consumption.py
+++ src/vehicle_models/energy_consumption.py
@@ -1,39 +1,31 @@
 import numpy as np
 
-def process_data(edge_data)-> list:
-    '''
-    Function that takes in an edge and uses the data to process values and send them into the consumption model.
-    '''
-    pass
 
-
-def physical_model(data):
+def physical_model(vehicle_data, static_data, road_data):
     '''
     calculate power required to move the vehicle along specified route
     '''
-    static_data = data["static_data"]
-    vehicle_data = data["vehicle_data"]
-    road_data = data["road_data"]
+    avg_incline_angle = np.radians(road_data["avg_incline_angle"])
 
     #Force from air resistance
     drag_force = 0.5 * static_data["air_dens"] * vehicle_data["frontal_area"] * vehicle_data["drag_coeff"] * road_data["velocity"]**2
-
     #Force from overcoming incline, or rolling down incline
-    grav_force = vehicle_data["mass"] * static_data["grav_acc"] * np.sin(road_data["incline_angle"])
+    grav_force = vehicle_data["mass"] * static_data["grav_acc"] * np.sin(avg_incline_angle)
 
     #Force opposing motion due to friction
-    roll_res_force = vehicle_data["mass"] * static_data["grav_acc"] * np.cos(road_data["incline_angle"]) * vehicle_data["roll_res"]
-
+    roll_res_force = vehicle_data["mass"] * static_data["grav_acc"] * np.cos(avg_incline_angle) * vehicle_data["roll_res"]
     #force needed to overcome these resistances and move the vehicle
     tract_force = drag_force + grav_force + roll_res_force + (vehicle_data["mass"] * road_data["acceleration"])
-
-    tract_power = tract_force * road_data["velocity"]
-
+    tract_power = max(0, tract_force * road_data["velocity"])
     return tract_power
 
-def battery_model(data):
-    tract_power = physical_model(data)
-    vehicle_params = data["vehicle_data"]
+def battery_model(battery_data, vehicle_data, static_data, road_data):
+    tract_power = physical_model(vehicle_data, static_data, road_data)
+
+
+
+
+
     OCV = vehicle_params["OCV"]  # Open circuit voltage
     R1_t = vehicle_params["R1_t"]  # Resistance 1
     R2_t = vehicle_params["R2_t"]  # Resistance 2
@@ -45,15 +37,9 @@ def battery_model(data):
 
 
     denominator = (OCV) * ns * np
-
     I_t = tract_power / denominator * (eta_em * eta_pe) ** np.sign(tract_power)
 
     return I_t
-
-
-
-
-
 
 def get_edge_consumption(params: dict)-> float:
     '''
